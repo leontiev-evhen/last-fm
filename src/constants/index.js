ee71022e929af41b75766246b0e6076179079f4f
Yevhen Leontiev <leo@mail.cz>
2019-02-14 21:40:12 +0200

    initial start

---
+++ src/constants/index.js
@@ -0,0 +1,4 @@
+const API_VERSION = '2.0';
+const FORMAT = 'json';
+const API_KEY = 'e715cc4e974dd68487208b6198b1f6a1';
+export const API_URL = `http://ws.audioscrobbler.com/${API_VERSION}/?api_key=${API_KEY}&format=${FORMAT}`;
